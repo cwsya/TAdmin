ff513307bc276fbea36fe41e2592daac2cbd25e8
陈文生 <2771628927@qq.com>
2022-04-26 09:55:32 +0800

    亿点bug

--- src/main/java/org/cwsya/tadmin/controller/UserController.java
+++ src/main/java/org/cwsya/tadmin/controller/UserController.java
@@ -56,7 +56,8 @@ public class UserController {
     }
     @SaCheckRole("admin")
     @PostMapping("/stopUser")
-    public Result<?> stopUser(@RequestBody Integer id) throws ParameterException {
+    public Result<?> stopUser(@RequestBody JSONObject jsonObject) throws ParameterException {
+        int id = jsonObject.getInt("id");
         if (StrUtil.isBlankIfStr(id)){
             throw new ParameterException();
         }
@@ -65,7 +66,8 @@ public class UserController {
     }
     @SaCheckRole("admin")
     @PostMapping("/startUser")
-    public Result<?> startUser(@RequestBody Integer id) throws ParameterException {
+    public Result<?> startUser(@RequestBody JSONObject jsonObject) throws ParameterException {
+        int id = jsonObject.getInt("id");
         if (StrUtil.isBlankIfStr(id)){
             throw new ParameterException();
         }
